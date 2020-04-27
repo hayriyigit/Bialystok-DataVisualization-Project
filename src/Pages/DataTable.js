16ce85b41792b3f6c68311f1c6627dc72d556c67
Francisco Molina <molinajimenez01@gmail.com>
2020-04-27 22:46:15 +0200

    fixed conversion to float

--- src/Pages/DataTable.js
+++ src/Pages/DataTable.js
@@ -3,37 +3,37 @@ import { HotTable } from '@handsontable/react';
 
 
 export default class DataTable extends Component {
-    
+
     componentDidMount = () => {
         console.log(this.props.location.state)
         this.identifyColumns(this.props.location.state.CsvFile)
     }
 
-    identifyColumns(data){
+    identifyColumns(data) {
         //we should check its the correct csv data :p
         let keys = Object.keys(data[0])
         console.log(data[0])
         // we print header columns..
-        for(let i = 0; i<keys.length; i++){
+        for (let i = 0; i < keys.length; i++) {
             //values of first row
-            let val = parseInt(data[0][keys[i]])
-            if (isNaN(val)){
+            let val = parseFloat(data[0][keys[i]])
+            if (isNaN(val)) {
                 val = data[0][keys[i]]
             }
-            console.log(val)
+            
         }
     }
     render() {
         return (
             <div>
-                        <HotTable
-                            id="hot"
-                            data={this.props.location.state.CsvFile}
-                            colHeaders={true}
-                            rowHeaders={true}
-                            columnSorting={true}
-                            licenseKey = 'non-commercial-and-evaluation'
-                        />
+                <HotTable
+                    id="hot"
+                    data={this.props.location.state.CsvFile}
+                    colHeaders={true}
+                    rowHeaders={true}
+                    columnSorting={true}
+                    licenseKey='non-commercial-and-evaluation'
+                />
             </div>
         )
     }
