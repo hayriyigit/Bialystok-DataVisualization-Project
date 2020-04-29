20adbff812a6a4521c52f4fc7a0072c92626e66f
Francisco Molina <molinajimenez01@gmail.com>
2020-04-30 01:47:13 +0200

    idk why this is happeniiing
    sssssssssss

--- src/Pages/DataTable.js
+++ src/Pages/DataTable.js
@@ -2,27 +2,47 @@ import React from 'react'
 import { HotTable } from '@handsontable/react';
 import Navbar from '../components/Navbar'
 
- const DataTable = props => {
-    const FileOptions = props.location.state.FileOptions
-    const DataSet = props.location.state.CsvFile
-
-    const settings = {
-        data : FileOptions.header ? DataSet.body : DataSet,
-        colHeaders: FileOptions.header ? DataSet.head : true,
-        rowHeaders: true,
-        dropdownMenu: true,
-        filters: true,
-        licenseKey: "non-commercial-and-evaluation"
+export default class DataTable extends React.Component {
+    constructor(props) {
+        super(props)
+        this.HotTable = React.createRef()
     }
 
-    return (
-        <div>
-            <Navbar/>
-            <HotTable
-                settings = {settings}
-            />
-        </div>
-    )
+
+    componentDidMount() {
+        console.log(this.HotTable)
+    }
+
+    getSelectedData=()=>{
+        console.log(this.HotTable.getSelected)
+    }
+
+    render(){
+
+        const FileOptions = this.props.location.state.FileOptions
+        const DataSet = this.props.location.state.CsvFile
+
+        const settings = {
+            data: FileOptions.header ? DataSet.body : DataSet,
+            colHeaders: FileOptions.header ? DataSet.head : true,
+            rowHeaders: true,
+            dropdownMenu: true,
+            filters: true,
+            licenseKey: "non-commercial-and-evaluation",
+            outsideClickDeselects: false,
+            selectionMode: 'multiple', // 'single', 'range' or 'multiple'
+        }
+        return(
+            <div>
+        <Navbar />
+        <HotTable
+            settings={settings}
+            ref={this.HotTable}
+        />
+            <button onClick={this.getSelectedData()}>AAAA</button>
+            </div >
+        )
+    }
+    
 }
 
-export default DataTable;
