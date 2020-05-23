3eb7b3b33045f2cd648649cf2aee0202668f9312
Francisco Molina <molinajimenez01@gmail.com>
2020-05-23 14:08:33 +0200

    indent fix

--- src/components/ModalRender.js
+++ src/components/ModalRender.js
@@ -8,22 +8,22 @@ export default class ModalRender extends Component {
         return (
           <BarChartModal show={this.props.show} onHide={this.props.onHide} />
         );
-        break;
+        
       case "Histogram":
         return (
           <HistogramModal show={this.props.show} onHide={this.props.onHide} />
         );
-        break;
+        
       case "Line Chart":
         return (
           <LineChartModal show={this.props.show} onHide={this.props.onHide} />
         );
-        break;
+        
       case "Pie Chart":
         return (
           <PieChartModal show={this.props.show} onHide={this.props.onHide} />
         );
-        break;
+        
 
       default:
         return null;
--- src/components/Modals.js
+++ src/components/Modals.js
@@ -6,41 +6,41 @@ import ModalFooter from "react-bootstrap/ModalFooter";
 import ModalTitle from "react-bootstrap/ModalTitle";
 
 export const BarChartModal = (props) => (
-    <Modal show={props.show} onHide={props.onHide}>
-            <ModalHeader>
-              <ModalTitle>Bar Chart</ModalTitle>
-            </ModalHeader>
-            <ModalBody>Some Chart Settings</ModalBody>
-            <ModalFooter>This is the footer</ModalFooter>
-          </Modal>
+  <Modal show={props.show} onHide={props.onHide}>
+    <ModalHeader>
+      <ModalTitle>Bar Chart</ModalTitle>
+    </ModalHeader>
+    <ModalBody>Some Chart Settings</ModalBody>
+    <ModalFooter>This is the footer</ModalFooter>
+  </Modal>
 );
 
 export const HistogramModal = (props) => (
-    <Modal show={props.show} onHide={props.onHide}>
-            <ModalHeader>
-              <ModalTitle>HistogramModal</ModalTitle>
-            </ModalHeader>
-            <ModalBody>Some Chart Settings</ModalBody>
-            <ModalFooter>This is the footer</ModalFooter>
-          </Modal>
+  <Modal show={props.show} onHide={props.onHide}>
+    <ModalHeader>
+      <ModalTitle>HistogramModal</ModalTitle>
+    </ModalHeader>
+    <ModalBody>Some Chart Settings</ModalBody>
+    <ModalFooter>This is the footer</ModalFooter>
+  </Modal>
 );
 
 export const LineChartModal = (props) => (
-    <Modal show={props.show} onHide={props.onHide}>
-            <ModalHeader>
-              <ModalTitle>LineChartModal</ModalTitle>
-            </ModalHeader>
-            <ModalBody>Some Chart Settings</ModalBody>
-            <ModalFooter>This is the footer</ModalFooter>
-          </Modal>
+  <Modal show={props.show} onHide={props.onHide}>
+    <ModalHeader>
+      <ModalTitle>LineChartModal</ModalTitle>
+    </ModalHeader>
+    <ModalBody>Some Chart Settings</ModalBody>
+    <ModalFooter>This is the footer</ModalFooter>
+  </Modal>
 );
 
 export const PieChartModal = (props) => (
-    <Modal show={props.show} onHide={props.onHide}>
-            <ModalHeader>
-              <ModalTitle>PieChartModal</ModalTitle>
-            </ModalHeader>
-            <ModalBody>Some Chart Settings</ModalBody>
-            <ModalFooter>This is the footer</ModalFooter>
-          </Modal>
+  <Modal show={props.show} onHide={props.onHide}>
+    <ModalHeader>
+      <ModalTitle>PieChartModal</ModalTitle>
+    </ModalHeader>
+    <ModalBody>Some Chart Settings</ModalBody>
+    <ModalFooter>This is the footer</ModalFooter>
+  </Modal>
 );
